ee3342ad49a12962eec9f39ef45e317fc316ef3e
intellerX <anmevel10@gmail.com>
2020-07-01 02:32:29 -0500

    commitDesk

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -6,6 +6,8 @@ import Carousel from '../components/Carousel';
 import CarouselItem from '../components/CarouselItem';
 import '../assets/styles/components/Home.scss';
 import Image1 from '../assets/static/Image11.jpg'
+import Image2 from '../assets/static/Captura.png'
+import Image3 from '../assets/static/Captura2.png'
 
 import '../assets/styles/App.scss';
 
@@ -17,25 +19,31 @@ const Home = ({ mylist, trends, originals }) => {
 	return (
 		<div className = "home">
 			
-			<h1 className = "title">La Casita de Magy</h1>
+			<h1 className = "title">Desk Corp</h1>
 			
 			<img className = "image1" id="Image1"   data-type="image"  src={Image1}></img>
 
 			<div>
-				<h1 className= "title__2">La misión de La Casita de Magy</h1>
-				<h3>Educacion unica y valiosa</h3>
+				<h1 className= "title__2">Misión</h1>
+				<h3>Un Futuro sostenible</h3>
 				<p className = "par">
-				Creemos que los niños prosperan en un ambiente que valora su individualidad, y al mismo tiempo les brinda apoyo y oportunidades para crecer emocional, social y creativamente. Al desarrollar las fortalezas, intereses y la curiosidad de cada niño, los guiamos a explorar el mundo que los rodea, así como a desarrollar nuevas habilidades y formar relaciones estrechas.
+					Somos un grupo de emprendedores(as) e ingenieros(as) llenos de talento con una idea innovadora que esperamos que contribuya a tener un futuro mejor. Ofrecemos soluciones inteligentes para empresas de cualquier tamaño y nos llena de orgullo la inigualable dedicación que ponemos en todos nuestros servicios.
+					En Desk Corp creemos que el conocimiento y las ventajas tecnológicas adecuados pueden llevar al éxito de las empresas. Siempre buscamos la opinión de nuestros clientes para aprender y evolucionar. Contáctanos hoy mismo para concretar una reunión con uno de nuestros representantes de ventas o para solicitar una demostración.
 				</p>
 			</div>
 
-			<Categories title='Publicaiones'>
+			<Categories title='Productos'>
 				<Carousel className = "carousel">
 					{trends.map((item) => (
 						<CarouselItem key={item.id} {...item}  />
 					))}
 				</Carousel>
 			</Categories>
+
+			<img className = "image2" id="Image2"   data-type="image"  src={Image2}></img>
+
+			<img className = "image2" id="Image2"   data-type="image"  src={Image3}></img>
+
 
 			
 		</div>
